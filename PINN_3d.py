f2bbb7e2a9a2f7febbcbf5a6ce9b5674d3d1f9c5
Filippo Sottovia <sottoviafilippo@gmail.com>
2026-06-16 22:15:36 +0200

    work in progresss

--- PINN_3d.py
+++ PINN_3d.py
@@ -2,6 +2,7 @@
 # obviously redundant work. written by me to understand PINNs and how they are built from the ground up
 
 import numpy as np
+from numpy.random import uniform
 
 
 class PINN_heat_3d:
@@ -10,3 +11,16 @@ class PINN_heat_3d:
     """
 
     # initialisation: see Glorot initilisation (good for tanh/sigmoid)
+    # the biases b in the pass forward equation can be initialized to 0
+
+    def __init__(self, N_layers: int, N_nodes: int, input_dim = 3, output_dim = 1):
+        
+        self.N_nodes = [3] + [N_nodes] * N_layers + [1] # number of nodes in each layer. The first layer corresponds to the entry data, the last one to the output
+
+        self.weights = []
+        for k in range(N_layers):
+            a = np.sqrt(6./(self.N_nodes[k] + self.N_nodes[k+1]))# half-length of the interval from which we draw the uniform distribution
+            self.weights.append(np.random.uniform(-a, a, size = N_nodes))
+        
+        pass
+        
